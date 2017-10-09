c84402afbfef5a27f6f0ce66581f50685762833c
Matt McMahon <matt@broken-toys.com>
2017-10-09 12:22:27 -0400

    Changes to Invariant ====================
    More changes to Invariant in preperation for lazy-evaluation:
    1. Invariant has a constructor property. This was oririginally added to
       the Invariant function itself, and not to the object that the function
       returns (which is where we need it).
    2. Refactored invariantFrom; removed switch statement.
    
    Changes to be committed:
    -  Modified:   src/invariant/invariant.js

--- src/invariant/invariant.js
+++ src/invariant/invariant.js
@@ -6,71 +6,67 @@ const { AssertionError } = require('assert')
 const { getDefaultPredicate } = require('./predicate')
 const getDefaultMessage = require('./message')
 
-const invariantFromObject = block => {
-  const run = () => {
-    const {
-      expected,
-      actual,
-      predicate,
-      message
-    } = i
+const invariantFrom = {
+  'object' (block) {
+    const run = () => {
+      const {
+        expected,
+        actual,
+        predicate,
+        message
+      } = i
 
-    const result = expected
-      ? predicate(expected, actual)
-      : predicate(actual)
+      const result = expected
+        ? predicate(expected, actual)
+        : predicate(actual)
 
-    if (result === true) {
-      return Success(Object.assign({}, block, {
-        message,
-        result
-      }))
-    } else {
-      return Fail(Object.assign({}, block, {
-        message,
-        result: new AssertionError({
-          actual, expected, message
-        })
-      }))
+      if (result === true) {
+        return Success(Object.assign({}, block, {
+          message,
+          result
+        }))
+      } else {
+        return Fail(Object.assign({}, block, {
+          message,
+          result: new AssertionError({
+            actual, expected, message
+          })
+        }))
+      }
     }
+
+    const i = Object.assign({}, block, {
+      message: getDefaultMessage(block),
+      predicate: getDefaultPredicate(block),
+      run: run
+    })
+
+    return i.run()
+  },
+  'string' (comment) {
+    return Success(Object.assign({}, {
+      message: comment,
+      result: true
+    }))
+  },
+  'function' (predicate) {
+    Success(Object.assign({}, {
+      message: `Invariant(${predicate.name})`,
+      result: predicate()
+    }))
   }
-
-  const i = Object.assign({}, block, {
-    message: getDefaultMessage(block),
-    predicate: getDefaultPredicate(block),
-    run: run
-  })
-
-  return i.run()
 }
 
-const invariantFromString = comment =>
-Success(Object.assign({}, {
-  message: comment,
-  result: true
-}))
-
-const invariantFromFunction = predicate =>
-Success(Object.assign({}, {
-  message: `Invariant(${predicate.name})`,
-  result: predicate()
-}))
-
 const Invariant = description => {
-  switch (typeof description) {
-    case 'string':
-      return invariantFromString(description)
-    case 'function':
-      return invariantFromFunction(description)
-    case 'object':
-    default:
-      return invariantFromObject(description)
-  }
+  const i = invariantFrom[typeof description](description)
+  return Object.defineProperties(i, {
+    constructor: {
+      value: Invariant
+    }
+  })
 }
 
 Object.defineProperties(Invariant, {
-  constructor: {
-    value: Invariant
-  },
   of: {
     value: description => Invariant(description)
   }
