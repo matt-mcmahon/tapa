6cab349e013a03bdda2b7e8fcf2c1b43b8acd7fe
Matt McMahon <contact@matt-mcmahon.com>
2019-06-07 13:09:49 -0400

    Updated Type Defs

--- src/describe/index.d.ts
+++ src/describe/index.d.ts
@@ -1,1 +1,34 @@
-export * from "./describe"
+export interface Invariant<T> {
+  given: string
+  should: string
+  actual: T
+  expected: T
+}
+
+export interface Iterator<T> {
+  done: boolean
+  next(): T | undefined
+}
+
+export interface State {
+  description: string
+  total: number
+  pass: number
+  fail: number
+  invariants: Invariant<any>[]
+  /**
+   * returns the current __State__
+   */
+  valueOf(): State
+  [Symbol.iterator]: Iterator<State>
+}
+
+export interface Plan {
+  (assert: Function): Promise<any>
+}
+
+export function describe(): (
+  description: string,
+  plan: Plan,
+  state?: State
+) => void
