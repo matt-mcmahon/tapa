ed60e11f70916b36c974a258d2e5c8791474b940
Matt McMahon <contact@matt-mcmahon.com>
2019-07-04 23:30:05 -0400

    Edit: status.test.js
    - More readable testing.

--- src/status/status.test.js
+++ src/status/status.test.js
@@ -12,164 +12,50 @@ import {
 } from "./status"
 
 describe("status module", async assert => {
-  {
-    const i = Promise.resolve({
-      status: passing,
-    })
-
-    const given = inspect`${i}`
+  const promise = Promise.resolve({ status: passing })
+  const tests = [
     {
-      const should = inspect`be pending`
-      const actual = isPending(i)
-      const expected = true
-      assert({ given, should, actual, expected })
-    }
-
+      value: promise,
+      expected: [true, false, false],
+    },
     {
-      const should = inspect`not be passing`
-      const actual = isPassing(i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
-
+      value: { status: pending },
+      expected: [true, false, false],
+    },
     {
-      const should = inspect`not be failing`
-      const actual = isFailing(i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
-
+      value: await promise,
+      expected: [false, true, false],
+    },
     {
-      const should = inspect`resolve to passing`
-      const actual = isPassing(await i)
-      const expected = true
-      assert({ given, should, actual, expected })
-    }
-
+      value: { status: failing },
+      expected: [false, false, true],
+    },
     {
-      const should = inspect`resolve not resolve to failing`
-      const actual = isFailing(await i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
-
+      value: pending,
+      expected: [true, false, false],
+    },
     {
-      const should = inspect`resolve not resolve to pending`
-      const actual = isPending(await i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
-  }
-
-  {
-    const i = { status: failing }
-    const given = inspect`${i}`
-
+      value: passing,
+      expected: [false, true, false],
+    },
     {
-      const should = inspect`not be pending`
-      const actual = isPending(i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
-
+      value: failing,
+      expected: [false, false, true],
+    },
     {
-      const should = inspect`not be passing`
-      const actual = isPassing(i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
+      value: undefined,
+      expected: [false, false, false],
+    },
+  ]
 
-    {
-      const should = inspect`be failing`
-      const actual = isFailing(i)
-      const expected = true
-      assert({ given, should, actual, expected })
-    }
-  }
-
-  {
-    const i = { status: pending }
-    const given = inspect`${i}`
-
-    {
-      const should = inspect`be pending`
-      const actual = isPending(i)
-      const expected = true
-      assert({ given, should, actual, expected })
-    }
-
-    {
-      const should = inspect`not be passing`
-      const actual = isPassing(i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
-
-    {
-      const should = inspect`not be failing`
-      const actual = isFailing(i)
-      const expected = false
-      assert({ given, should, actual, expected })
-    }
-  }
-
-  {
-    const given = inspect`isPending(${undefined})`
-    const should = inspect`return false`
-    const actual = isPending()
-    const expected = false
-    assert({ given, should, actual, expected })
-  }
-
-  {
-    const given = inspect`isPassing(${undefined})`
-    const should = inspect`return false`
-    const actual = isPassing()
-    const expected = false
-    assert({ given, should, actual, expected })
-  }
-
-  {
-    const given = inspect`isFailing(${undefined})`
-    const should = inspect`return false`
-    const actual = isFailing()
-    const expected = false
-    assert({ given, should, actual, expected })
-  }
-
-  {
+  tests.forEach(({ value, expected }) => {
     const actual = [
-      isPending(pending),
-      isPassing(pending),
-      isFailing(pending),
+      isPending(value),
+      isPassing(value),
+      isFailing(value),
     ]
-    const expected = [true, false, false]
-    const given = inspect`apply ${"pending"} to [isFailing, isPassing, isFailing]`
+    const given = inspect`${value} to [isPending, isPassing, isFailing]`
     const should = inspect`be ${expected}`
     assert({ given, should, actual, expected })
-  }
-
-  {
-    const actual = [
-      isPending(passing),
-      isPassing(passing),
-      isFailing(passing),
-    ]
-    const expected = [false, true, false]
-    const given = inspect`apply ${"passing"} to [isFailing, isPassing, isFailing]`
-    const should = inspect`be ${expected}`
-    assert({ given, should, actual, expected })
-  }
-
-  {
-    const actual = [
-      isPending(failing),
-      isPassing(failing),
-      isFailing(failing),
-    ]
-    const expected = [false, false, true]
-    const given = inspect`apply ${"failing"} to [isFailing, isPassing, isFailing]`
-    const should = inspect`be ${expected}`
-    assert({ given, should, actual, expected })
-  }
+  })
 })
