cf5315cb1e94ec60a213f2c31fabea8431f85336
Matt McMahon <contact@matt-mcmahon.com>
2019-07-06 01:04:13 -0400

    Edit: state
    - New Implementation, uses iteration protocol.

--- src/state/state.js
+++ src/state/state.js
@@ -1,58 +1,83 @@
 import { isPending, isPassing, isFailing } from "../status"
 
-const reducer = (summary, assertion) => {
-  summary.pending += isPending(assertion) ? 1 : 0
-  summary.passing += isPassing(assertion) ? 1 : 0
-  summary.failing += isFailing(assertion) ? 1 : 0
-  summary.total += 1
-  return summary
-}
-
 const accumulator = () => ({
   pending: 0,
   passing: 0,
   failing: 0,
-  total: 0,
+  length: 0,
 })
 
-export const state = (name, ...assertions) => {
-  return new State({ name, assertions })
+const reducer = (summary, assertion) => {
+  summary.pending += isPending(assertion) ? 1 : 0
+  summary.passing += isPassing(assertion) ? 1 : 0
+  summary.failing += isFailing(assertion) ? 1 : 0
+  summary.length += 1
+  return summary
+}
+
+export const state = (label, ...assertions) => {
+  return new State({ label, assertions })
 }
 
 class State {
-  constructor({ name, assertions, history = [] }) {
-    this.name = name
-    this.history = Object.freeze(history)
-    this.assertions = Object.freeze(assertions)
-    Object.freeze(this)
+  constructor({
+    label = "",
+    assertions = [],
+    done = false,
+    history = [],
+  } = {}) {
+    this.assertions = assertions
+    this.done = done
+    this.history = history
+    this.label = label
+    this.summary = assertions.reduce(reducer, accumulator())
+  }
+
+  [Symbol.iterator]() {
+    return this.history[Symbol.iterator]()
+  }
+
+  [Symbol.asyncIterator]() {
+    return this
   }
 
   get length() {
-    return this.history.length
+    return this.assertions.length
   }
 
-  get summary() {
-    return this.assertions.reduce(reducer, accumulator())
-  }
+  next(...assertions) {
+    const promiseWhenDone = () =>
+      Promise.resolve({
+        done: true,
+        value: undefined,
+      })
 
-  add(...assertions) {
-    return new State({
-      name: this.name,
-      assertions: [...this.assertions, ...assertions],
-      history: [...this.history, this],
-    })
-  }
-
-  get promise() {
-    return Promise.all(this.assertions).then(
-      assertions =>
-        new State({
-          assertions,
-          name: this.name,
+    const promiseWhenResolved = () =>
+      Promise.all(this.assertions).then(assertions => ({
+        done: false,
+        value: new State({
+          label: this.label,
+          assertions: assertions,
+          done: true,
           history: [...this.history, this],
-        })
-    )
-  }
+        }),
+      }))
 
-  static of = state
+    const promiseWhenMore = () =>
+      Promise.resolve({
+        done: false,
+        value: new State({
+          label: this.label,
+          assertions: [...this.assertions, ...assertions],
+          history: [...this.history, this],
+          done: false,
+        }),
+      })
+
+    return this.done
+      ? promiseWhenDone()
+      : assertions.length === 0
+      ? promiseWhenResolved()
+      : promiseWhenMore()
+  }
 }
--- src/state/state.test.js
+++ src/state/state.test.js
@@ -1,25 +1,29 @@
-import { iife } from "@mwm/functional"
 import { describe } from "riteway"
-import { state as indexExport } from "."
+import { iife, prop } from "@mwm/functional"
+
 import { inspect } from "../inspect"
-import { failing, passing } from "../status"
+import { passing, failing } from "../status"
+
 import { state } from "./state"
+import { state as indexExport } from "."
 
 describe("state module", async assert => {
   {
-    const given = inspect`module named ${"./state"}`
-    const should = inspect`have an export named ${state}`
-    const actual = typeof state
-    const expected = "function"
-    assert({ given, should, actual, expected })
-  }
+    {
+      const given = inspect`module named ${"./state"}`
+      const should = inspect`have an export named ${state}`
+      const actual = typeof state
+      const expected = "function"
+      assert({ given, should, actual, expected })
+    }
 
-  {
-    const given = inspect`index export`
-    const should = inspect`be identical to named export`
-    const actual = indexExport
-    const expected = state
-    assert({ given, should, actual, expected })
+    {
+      const given = inspect`index export`
+      const should = inspect`be identical to named export`
+      const actual = indexExport
+      const expected = state
+      assert({ given, should, actual, expected })
+    }
   }
 
   const a = ({ actual, expected }) => ({
@@ -42,80 +46,161 @@ describe("state module", async assert => {
     ]
   })
 
-  const s7 = state("s7", ...examples)
+  const s0 = state("my-state", ...examples)
 
   {
-    const actual = s7.name
-    const expected = "s7"
-    const given = inspect`s7.name`
-    const should = inspect`be ${actual}`
-    assert({ given, should, actual, expected })
-  }
-
-  {
-    const { summary: actual } = s7
-    const expected = {
-      pending: 3,
-      passing: 3,
-      failing: 1,
-      total: 7,
-    }
-    const given = inspect`state with values and promises`
-    const should = inspect`have status ${expected}`
-    assert({ given, should, actual, expected })
-  }
-
-  const s7p = await s7.promise
-
-  {
-    const { summary, history } = s7p
-
     {
-      const actual = summary
-      const expected = {
-        pending: 0,
-        passing: 5,
-        failing: 2,
-        total: 7,
-      }
-      const given = inspect`resolved summary`
+      const actual = s0.label
+      const expected = "my-state"
+      const given = inspect`s0.label`
       const should = inspect`be ${expected}`
       assert({ given, should, actual, expected })
     }
 
     {
-      const actual = history
-      const expected = [s7]
-      const given = inspect`resolved history`
-      const should = inspect`be ${["s7"]}`
+      const actual = s0.length
+      const expected = 7
+      const given = inspect`s0.length`
+      const should = inspect`be ${expected}`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = s0.summary
+      const expected = {
+        pending: 3,
+        passing: 3,
+        failing: 1,
+        length: 7,
+      }
+      const given = inspect`s0.summary`
+      const should = inspect`have summary ${expected}`
       assert({ given, should, actual, expected })
     }
   }
 
   const iH = a({ actual: "H", expected: "H" })
-  const s8 = s7.add(iH)
-  const s8p = await s8.promise
+  const s1promise = s0.next(iH)
+  const { value: s1, done: s1done } = await s1promise
 
   {
-    const { summary, history } = await s8p
     {
-      const actual = summary
-      const expected = {
-        pending: 0,
-        passing: 6,
-        failing: 2,
-        total: 8,
-      }
-      const given = inspect`an additional assertion ${iH}`
-      const should = inspect`update status`
+      const actual = s1promise instanceof Promise
+      const expected = true
+      const given = inspect`s1.next()`
+      const should = inspect`should return a ${Promise}`
       assert({ given, should, actual, expected })
     }
 
     {
-      const actual = history
-      const expected = [s7, s8]
-      const given = inspect`history after add(${iH})`
-      const should = inspect`be ${["s7", "s8"]}`
+      const actual = s1done
+      const expected = false
+      const given = inspect`s1`
+      const should = inspect`should not be done`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = s1.summary
+      const expected = {
+        pending: 3,
+        passing: 4,
+        failing: 1,
+        length: 8,
+      }
+      const given = inspect`s1.summary`
+      const should = inspect`be ${expected}`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = [...s1]
+      const expected = s1.history
+      const given = inspect`[...s1]`
+      const should = inspect`be s1.history`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = [...s1]
+      const expected = [s0]
+      const given = inspect`s1.history`
+      const should = inspect`be ${expected.map(
+        prop("name")
+      )}`
+      assert({ given, should, actual, expected })
+    }
+  }
+
+  const s2promise = s1.next()
+  const { value: s2, done: s2done } = await s2promise
+
+  {
+    {
+      const actual = s2promise instanceof Promise
+      const expected = true
+      const given = inspect`s2promise = s1.next()`
+      const should = inspect`be a ${Promise}`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = s2.summary
+      const expected = {
+        pending: 0,
+        passing: 6,
+        failing: 2,
+        length: 8,
+      }
+      const given = inspect`s2.summary`
+      const should = inspect`be ${expected}`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = [...s2]
+      const expected = [s0, s1]
+      const given = inspect`s2.history`
+      const should = inspect`should be ${expected.map(
+        prop("name")
+      )}`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = s2done
+      const expected = false
+      const given = inspect`s2`
+      const should = inspect`should not be done`
+      assert({ given, should, actual, expected })
+    }
+  }
+
+  const s3promise = s2.next()
+  const { value: s3, done: s3done } = await s3promise
+
+  {
+    {
+      const actual = s3promise instanceof Promise
+      const expected = true
+      const given = inspect`s2.next() as ${s3promise}`
+      const should = inspect`be a ${Promise}`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = s3
+      const expected = undefined
+      const given = inspect`s3`
+      const should = inspect`should be ${expected}`
+      assert({ given, should, actual, expected })
+    }
+
+    {
+      const actual = s3done
+      const expected = true
+      const given = inspect`s3`
+      const should = inspect`should be done`
       assert({ given, should, actual, expected })
     }
   }
