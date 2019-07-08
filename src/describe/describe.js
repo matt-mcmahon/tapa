e39fce5514aeb17a6f163fc4ef34008165e35ad9
Matt McMahon <contact@matt-mcmahon.com>
2019-07-08 15:59:54 -0400

    Edit: describe
    - Edited to reflect changes to state.

--- src/describe/describe.js
+++ src/describe/describe.js
@@ -1,74 +1,21 @@
-import deepEqual from "deep-equal"
+import { assert } from "../assert"
+import { state } from "../state"
 
-import { captureStack } from "../capture-stack"
-import { print } from "./print"
-
-const pass = () => {
-  return { pass: true }
+const execute = async plan => {
+  const assertions = []
+  return plan(block => {
+    assert(block).then(assertion =>
+      assertions.push(assertion)
+    )
+  }).then(() => assertions)
 }
 
-const fail = () => {
-  return {
-    fail: true,
-    stack: captureStack(describe),
-  }
-}
-
-const update = status => invariant => {
-  status.total++
-  invariant.fail === true && status.fail++
-  invariant.pass === true && status.pass++
-  status.invariants.push(invariant)
-}
-
-const makeAssert = update => ({
-  given,
-  should,
-  actual,
-  expected,
-  ...rest
-}) => {
-  const result = deepEqual(actual, expected)
-    ? pass()
-    : fail()
-
-  const invariant = {
-    ...rest, // spread first so we overwrite
-    given,
-    should,
-    message: `given ${given}; should ${should}`,
-    actual,
-    expected,
-    ...result, // result overwrites rest
-  }
-  update(invariant)
-}
-
-const describe = (description, plan) => {
-  const status = {
-    description,
-    total: 0,
-    pass: 0,
-    fail: 0,
-    invariants: [],
-  }
-  const updater = update(status)
-  const assert = makeAssert(updater)
-  assert.pass = message => updater({ message, ...pass() })
-  assert.fail = message => updater({ message, ...fail() })
-
-  const result = plan(assert)
-
-  return Promise.resolve(result)
-    .catch(err => updater({ fail: err }))
-    .finally(print(status))
-}
-
-export {
-  describe as default,
-  describe,
-  fail,
-  makeAssert,
-  pass,
-  update,
+export const describe = async (description, plan) => {
+  return execute(plan)
+    .then(assertions => state(description, ...assertions))
+    .then(state => state.next())
+    .catch(err => {
+      console.error(`Unexpected Error in ${description}`)
+      console.error(inspect`${err}`)
+    })
 }
--- src/describe/describe.test.js
+++ src/describe/describe.test.js
@@ -1,25 +1,58 @@
-import test from "tape"
-import { describe } from "./describe"
+import { describe as rw } from "riteway"
 import { describe as indexExport } from "."
+import { inspect } from "../inspect"
+import { describe } from "./describe"
 
-test("describe module", assert => {
+rw("describe module", async assert => {
   {
-    const given = 'module named "./describe"'
-    const should = 'have an export named "describe"'
-    const message = `given ${given}; should ${should}`
+    const given = inspect`module named ${"./describe"}`
+    const should = inspect`have an export named ${describe}`
     const actual = typeof describe
     const expected = "function"
-    assert.deepEqual(actual, expected, message)
+    assert({ given, should, actual, expected })
   }
 
   {
-    const given = 'index export "./describe"'
-    const should = "be identical to mamed export"
-    const message = `given ${given}; should ${should}`
+    const given = inspect`index export`
+    const should = inspect`be identical to named export`
     const actual = indexExport
     const expected = describe
-    assert.deepEqual(actual, expected, message)
+    assert({ given, should, actual, expected })
   }
 
-  assert.end()
+  const description = "my description"
+  const plan = async assert => {
+    {
+      const given = inspect`a test assertion`
+      const should = inspect`do something?`
+      const actual = "A"
+      const expected = "A"
+      assert({ given, should, actual, expected })
+    }
+  }
+  const d0 = describe
+  const d0promise = d0(description, plan)
+
+  {
+    const given = inspect`describe(${description}, ${plan})`
+    const should = inspect`be a ${Promise}`
+    const actual = d0promise instanceof Promise
+    const expected = true
+    assert({ given, should, actual, expected })
+  }
+
+  const s0 = await d0promise
+
+  {
+    const actual = s0.summary
+    const expected = {
+      pending: 0,
+      passing: 1,
+      failing: 0,
+      length: 1,
+    }
+    const given = inspect`resolved ${d0promise}.summary`
+    const should = inspect`be ${expected}`
+    assert({ given, should, actual, expected })
+  }
 })
